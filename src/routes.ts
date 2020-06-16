14edd18d5afd32e1b2b9721ab5a710290bcfae6a
Pedro Silva <pedro.silva-dev@hotmail.com>
2020-06-16 00:42:44 -0300

    Cadastro de Pontos (INSERT)

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import knex  from './database/connection';
+import express, { request } from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get("/items",async (request, response) => {
+routes.get("/items", async (request, response) => {
     const items = await knex('items').select('*');
 
     const serializedItems = items.map(item => {
@@ -16,5 +16,42 @@ routes.get("/items",async (request, response) => {
 
     return response.json(serializedItems)
 });
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const insertedIds = await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+    });
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id,
+        }
+    })
+
+    await knex('point_items').insert(pointItems);
+
+    return response.json({ succes: true });
+})
 
 export default routes;
